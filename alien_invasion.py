269d7bf5eb2a840e0ad8a374a70c999a6bc3fc75
JMPorras <porrasguerrajosemanuel@gmail.com>
2026-02-16 16:34:00 -0500

    Añadiendo lógica para manejar el estado del juego y colisiones de aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,9 +37,12 @@ class AlienInvasion:
 
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.settings.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+            
             self._update_screen()
             self.clock.tick(60)
      
@@ -123,22 +126,35 @@ class AlienInvasion:
         # Busca colisiones alien-nave.
         if pygame.sprite.spritecollideany(self.ship, self.aliens): # type: ignore
             self._ship_hit()
+        self._check_aliens_bottom()
+
+    def _check_aliens_bottom(self):
+        """Comprueba si algún alien ha llegado al fondo de la
+        pantalla."""
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= self.settings.screen_height:
+                # Trata esto como si la nave hubiese sido alcanzada.
+                self._ship_hit()
+                break
 
     def _ship_hit(self):
         """Responde al impacto de un alien en la nave."""
-        # Disminuye ships_left.
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            # Disminuye ships_left.
+            self.stats.ships_left -= 1
 
-        # Se deshace de los aliens y balas restantes.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Se deshace de los aliens y balas restantes.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Crea una flota nueva y centra la nave.
-        self._create_fleet()
-        self.ship.center_ship()
+            # Crea una flota nueva y centra la nave.
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Pausa.
-        sleep(0.5)
+            # Pausa.
+            sleep(0.5)
+        else:
+            self.settings.game_active = False
 
     def _fire_bullet(self):
         """Crea una nueva bala y la añade al grupo de balas."""
--- settings.py
+++ settings.py
@@ -8,11 +8,11 @@ class Settings:
         self.screen_width = 1000
         self.screen_height = 500
         self.bg_color = (230, 230, 230)
-        self.fullscreen = False
+        self.fullscreen = True
 
         # Configuración de las balas.
         self.bullet_speed = 4.0
-        self.bullet_width = 3000 # estaba en 3
+        self.bullet_width = 3 # estaba en 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
@@ -26,3 +26,4 @@ class Settings:
         # Configuraciones de estadísticas
         self.ship_speed = 2.0
         self.ship_limit = 3
+        self.game_active = True
